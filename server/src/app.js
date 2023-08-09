c5a84fe74e167c3da7dab23e5fe09d05bb84eecd
knight2003n <123630647+knight2003n@users.noreply.github.com>
2023-08-09 13:54:27 +0000

    folder perpared

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+let express = require('express')
+const app = express()
+
+app.get('/status',function(req,res){
+    res.send('Hello nodejs server thanawat mankhetkon')
+})
+
+app.get('/hello/:person',function(req,res){
+    console.log('hello - ' + req.params.person)
+    res.send('say hello with ' +req.params.person)
+})
+
+app.get('user/:userId',function(req,res){
+    res.send('ดูข้อมูลผู้ใช้')
+})
+
+app.get('/users', function(req,res){
+    res.send('เรียกข้อมูล ผู้ใช้ทั้งหมด')
+})
+
+let port = 8081
+app.listen(port, function(){
+    console.log('server running on ' + port)
+})
