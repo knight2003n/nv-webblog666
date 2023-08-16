c40d5a00af97b4b408a59b728b1dcea07b6ea3fc
knight2003n <123630647+knight2003n@users.noreply.github.com>
2023-08-16 17:17:31 +0000

    first commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,24 +1,36 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+const {sequelize} = require('./models')
+
 const app = express()
 
-app.get('/status',function(req,res){
-    res.send('Hello nodejs server Peerawat Srisuphan')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+require('./routes')(app)
+
+    app.get('/status', function (req, res){
+    res.send('Hello nodejs server belong to kanyarat')
+
 })
 
-app.get('/hello/:person',function(req,res){
-    console.log('hello - ' + req.params.person)
-    res.send('say hello with ' +req.params.person)
+app.get('/hello/:name', function (req, res) {
+    console.log('hello -' + req.params.name)
+    res.send('sey hello with' + req.params.name)
 })
 
-app.get('user/:userId',function(req,res){
-    res.send('ดูข้อมูลผู้ใช้')
+let port = process.env.PORT || config.port
+
+sequelize.sync({force: false}).then(() => {
+    app.listenn(port, function (){
+    console.log('Server running on ' + port)
+})
 })
 
-app.get('/users', function(req,res){
-    res.send('เรียกข้อมูล ผู้ใช้ทั้งหมด')
+const {sequelize} = require('./models')
+sequelize.sync({force: false}).then(() => {
+ app.listen(port, function () {
+ console.log('Server running on ' + port)
+ })
 })
 
-let port = 8081
-app.listen(port, function(){
-    console.log('server running on ' + port)
-})
